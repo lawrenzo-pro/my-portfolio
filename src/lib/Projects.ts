496b832f2d21c86bb87c71f85cafbd075fae4101
Lawrence Kimani <larrykimani01@gmail.com>
2023-09-30 15:58:41 +0300

    minor experimental tweaks

---
+++ src/lib/Projects.ts
@@ -0,0 +1,24 @@
+const projects = [
+	{
+		title: 'Lexi',
+		technologies: ['Rust'],
+		description:
+			'Lexi is a simple microkernel written in the rust programming language',
+		url: 'https://github.com/lawrenzo-pro/lexi'
+	},
+	{
+		title: 'Microgrep',
+		technologies: ['Rust'],
+		description:
+			'How about rewriting grep in rust and making it worse',
+		url: 'https://github.com/lawrenzo-pro/'
+	},
+	{
+		title: 'Lexi loader',
+		technologies: ['Assembly'],
+		description: 'A simple bootloader written in pure x86 assembly',
+		url: 'https://github.com/lawrenzo-pro/lexi-loader'
+	}
+];
+
+export default projects;
---
+++ src/lib/Skills.ts
@@ -0,0 +1,10 @@
+const skills = {
+	programming: ['Node.js', 'Rust', 'Java', 'Svelte.js', 'Go', 'Assembly'],
+	// devops: [],
+	databases: ['Mysql'],
+	// tools: [],
+	languages: ['Swahili', 'English'],
+	other: ['Design principles']
+};
+
+export default skills;
