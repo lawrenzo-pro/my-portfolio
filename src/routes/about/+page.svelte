f22dab126e4ea1ce55ed466900dc829bdc4c3143
lawrenzo-pro <57544775+lawrenzo-pro@users.noreply.github.com>
2024-06-04 12:03:09 +0300

    Update +page.svelte

--- src/routes/about/+page.svelte
+++ src/routes/about/+page.svelte
@@ -11,7 +11,7 @@
 		<h1>About</h1>
 		<p>
 			I am a Computer Science student experienced in Systems Programming in Rust.
-			I'm learning about embedded systems programming on AVR  microcontrollers and arm microcontrollers.
+			I'm learning about embedded systems programming on AVR microcontrollers and arm microcontrollers.
 
 I'm  also interested  in IoT security .
 I'm looking forward to steer embedded systems into using safer languages. 
