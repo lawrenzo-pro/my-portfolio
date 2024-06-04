99cd54a43062e3ed55ea69a7cb6bade60198cf6d
lawrenzo-pro <57544775+lawrenzo-pro@users.noreply.github.com>
2024-06-04 11:58:25 +0300

    Update +page.svelte

--- src/routes/about/+page.svelte
+++ src/routes/about/+page.svelte
@@ -11,7 +11,10 @@
 		<h1>About</h1>
 		<p>
 			I am a Computer Science student experienced in Systems Programming in Rust.
-			I'm also currently learning web technologies such as node.js with great interest in backend.
+			I'm learning about embedded systems programming on arrays microcontrollers and arm microcontrollers.
+
+I'm  also interested  in iot security .
+I'm looking forward to steer embedded systems into using safer languages. 
 		</p>
 		<h2>Skills</h2>
 		{#each Object.entries(skills) as [section, technologies]}
